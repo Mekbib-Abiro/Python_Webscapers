23e9b467d6fc9556ccfb1fc55acebebc67376f8c
Mekbib Abiro <133805365+Mekbib-Abiro@users.noreply.github.com>
2024-01-23 22:55:30 +0300

    crawling.py

--- crawling.py
+++ crawling.py
@@ -1,27 +1,38 @@
+'''
+This script crawls an entire site for a specific keyword and prints links conatining that keyword.
+
+The script requires a url as a command line argument to run. 
+It also accepts the keyword to be searched using the -p flag. 
+Default keyword is python.
+
+usage -> py ./crawling.py http://www.example.com -p keyword
+'''
+
+
 import re
 from urllib.parse import urljoin, urlparse
 import logging
-import requests
+from selenium import webdriver
+from selenium.common.exceptions import JavascriptException
 from bs4 import BeautifulSoup
-import http.client
 import argparse
 
+logging.basicConfig(level=logging.INFO)
 #Declare the default phrase
 DEFAULT_PHRASE = 'python'
 
-# Search for all elemnts on a page containing the text, 
+# Search for all elements on a page containing the text, 
 # and print the link containing the element and the whole element.
 def search_text(source_link, page, text):
-    for element in page.find_all(text= re.compile(text, re.IGNORECASE)):
-        print(f'Link {source_link}: --> {element}')
-        
+    for element in page.find_all(string= re.compile(text, re.IGNORECASE))[1:]:
+        print(f'Link {source_link}: --> {element.text}')
+
 
 # Find all the links on a page and return them
 def get_links(parsed_source, page):
     links = []
-    for element in page.find_all('a'):
-        link = element.get('href')
-        
+    for element in page.find_all('a', href=True):
+        link = element['href']
         # If element has no href attribute continue
         if not link:
             continue
@@ -42,31 +53,32 @@ def get_links(parsed_source, page):
             link = f'{scheme}://{netloc}{path}'
             
         links.append(link)
-    
     return links
 
 # Search the text in the page and in every link in that page
 def process_link(source_link, text):
-    logging.info(f'Extracting links from {source_link}:')
+    print(f'Extracting links from {source_link}:')
     
     # parse the source link on to its 6 building components,
     # for local hosts and domain checking
     parsed_source = urlparse(source_link)
     
-    # Downlaod the page
-    res = requests.get(source_link)
-    
-    # Skip broken links/Not found page/... and non html pags
-    if res.status_code != http.client.OK:
-        logging.error(f'Error retrieving {source_link}: {res}')
-        return  []
-    
-    if 'html' not in res.headers['Content-Type']:
-        logging.info(f'Link {source_link} is not an HTML page')
-        return []
+    # Download the page
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        driver.get(source_link)
+        html_content = driver.page_source
+        driver.execute_script('adsbygoogle.push();')
+    except JavascriptException as e:
+        pass
+    finally:
+        if 'driver' in locals():
+            driver.quit()
     
     # Parse the page
-    page = BeautifulSoup(res.text, 'html.parser')
+    page = BeautifulSoup(html_content, 'html.parser')
     
     # Search the text in that page
     search_text(source_link, page, text)
@@ -82,7 +94,7 @@ def Main(base_url, to_search):
     max_checks = 10
     
     # Loop until max checks = 0 or all the links are checked(no more links in to to_check list)
-    while bool(to_check and max_checks):
+    while to_check and max_checks:
         # Catch the most recent link in the to_check list and remove it from to check
         link = to_check.pop(0)
         
@@ -93,7 +105,7 @@ def Main(base_url, to_search):
         checked_links.add(link)
         
         # Make sure the link is checked and if not add it to to_check,
-        # and also to checked_links to make sure it is not checked more than once
+        # and also added to checked_links to make sure it is not checked more than once
         for link in links:
             if link not in checked_links:
                 checked_links.add(link)
@@ -101,6 +113,7 @@ def Main(base_url, to_search):
         
         # Decrement the max_checks in each while loop
         max_checks -= 1
+        
     
 # Make sure the whole script can not be imported    
 if __name__ == '__main__':
